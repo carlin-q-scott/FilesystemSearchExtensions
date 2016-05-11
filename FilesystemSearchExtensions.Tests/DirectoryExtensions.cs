9aa0c75c7c2b271d259cf466ab57047b8a9ffc6f
Carlin <carlin.scott@shiftwise.net>
2016-05-10 18:33:30 -0700

    Created GetDirectoriesRecursively and a unit test for it

---
+++ FilesystemSearchExtensions.Tests/DirectoryExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.IO.Fakes;
+using Microsoft.QualityTools.Testing.Fakes;
+using NUnit.Framework;
+
+namespace FilesystemSearchExtensions.Tests
+{
+    [TestFixture]
+    public class DirectoryExtensions
+    {
+        [Test]
+        public void GetDirectoriesRecursively_FindCurrentDirectorySibling()
+        {
+            using (ShimsContext.Create())
+            {
+                var currentDirectory = new ShimDirectoryInfo()
+                {
+                    GetDirectoriesString = (s) => new[] {new ShimDirectoryInfo() {NameGet = () => "sibling"}.Instance}
+                }.Instance;
+
+                var result = currentDirectory.GetDirectoriesRecursively("sibling");
+                Assert.That(result, Has.Length.EqualTo(1));
+            }
+        }
+    }
+}
---
+++ FilesystemSearchExtensions/DirectoryExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using System.Linq;
+
+namespace FilesystemSearchExtensions
+{
+    public static class DirectoryExtensions
+    {
+        /// <summary>
+        /// Finds the path to an ancestor directory, which is a directory that is a sibling of the current directory or its parent/grandparent/etc.
+        /// </summary>
+        /// <param name="searchPattern"></param>
+        /// <returns></returns>
+        public static DirectoryInfo[] GetDirectoriesRecursively(this DirectoryInfo currentDirectory, string searchPattern)
+        {
+            var matchingSubDirectories = currentDirectory.GetDirectories(searchPattern);
+
+            if (matchingSubDirectories.Length > 0) return matchingSubDirectories;
+
+            return GetDirectoriesRecursively(currentDirectory.Parent, searchPattern);
+        }
+
+        public static string[] GetDirectoriesRecursively(string searchPattern)
+        {
+            var currentDirectory = new DirectoryInfo(Directory.GetCurrentDirectory());
+
+            return GetDirectoriesRecursively(currentDirectory, searchPattern).Select(di => di.FullName).ToArray();
+        }
+    }
+}
